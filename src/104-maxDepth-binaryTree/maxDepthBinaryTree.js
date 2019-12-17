b8a68753863d44e060793e5449928a5ac0e02d34
nico <nico@pop-os.localdomain>
2019-12-17 22:25:07 +0700

    small comment maxDepthBST

--- src/104-maxDepth-binaryTree/maxDepthBinaryTree.js
+++ src/104-maxDepth-binaryTree/maxDepthBinaryTree.js
@@ -10,10 +10,12 @@
 
 var maxDepth = function(root) {
   if (root === null) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1; // adds +1 for every depth search
 };
 
 // Test case
 // binarytree = [3, 9, 20, null, null, 15, 7];
 // maxdepth(binaryTree);
 // output: 3
+
+
